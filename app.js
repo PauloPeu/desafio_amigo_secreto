fe066a4e9d31bda513d27c6d240aeae8cbd49768
Paulo <pcnbrito@hotmail.com>
2025-02-19 21:44:22 -0300

    Removidos os comentários e as linhas com console.log usado pra acompanhar os passos.

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//Jogo do Amigo Secreto
 
 let amigos = [];
 function adicionarAmigo() {
     if (document.querySelector('input').value == "") {
-        alert('Você Precisa digitar um nome!!');
-        
+        alert('Você Precisa digitar um nome!!');    
     }else{
         let nomes = document.querySelector('input').value;
         amigos.push(nomes);
@@ -17,18 +16,10 @@ function limparCampo() {
     limparNome.value = '';
 }
 function exibirNomes() {
-    // Array com os itens da lista
     let itens = amigos;
-  
-    // Seleciona a lista no HTML
     let lista = document.getElementById("listaAmigos");
-
     for (let cont = 0; cont < itens.length; cont++){
-        
-        // Limpa a lista atual
         lista.innerHTML = "";
-        
-        // Adiciona cada item do array à lista
         itens.forEach(function(item) {
             let li = document.createElement("li");
             li.textContent = item;
@@ -39,14 +30,13 @@ function exibirNomes() {
 function sortearAmigo() {
     let tamanhoLista = amigos.length;
     sorteador(tamanhoLista); 
-        function sorteador() {
-            if (amigos == "") {
-                let resultado = document.getElementById("resultado");
-                resultado.innerHTML = (`VOCÊ NÃO DIGITOU NENHUM NOME!!`);
-            }else{
-                let numeroSorteado = (Math.floor(Math.random() * (tamanhoLista - 0)));
-                resultado.innerHTML = (`Nome Sorteado: ${amigos[numeroSorteado]}`);
-                console.log(amigos[numeroSorteado], (numeroSorteado));
+    function sorteador() {
+        if (amigos == "") {
+            let resultado = document.getElementById("resultado");
+            resultado.innerHTML = (`VOCÊ NÃO DIGITOU NENHUM NOME!!`);
+        }else{
+            let numeroSorteado = (Math.floor(Math.random() * (tamanhoLista - 0)));
+            resultado.innerHTML = (`Nome Sorteado: ${amigos[numeroSorteado]}`);
             }
         }
 }
